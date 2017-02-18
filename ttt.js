9deba1e4518527ddf91bfb6b5205998ba6b6230b
Akshit Sahani <akshit_sahani@hotmail.com>
2017-02-18 17:47:18 -0500

    complete tictactoe assignment with coloring for each X or O. You should refactor the logic for checkwin function though

--- ttt.js
+++ ttt.js
@@ -1,76 +1,79 @@
 var count = 0;
 
-function varup(){
+function countup(){
   count += 1;
 }
 
-function ttt(){
-  if (count % 2 === 0){
-    $('.box').text('O');
+function reset(){
+  $('.box').empty();
+  $('.box').css('background-color', 'white');
+}
+
+function checkwin(str){
+  if (($('.one').html() === str) && ($('.two').html() === str) && ($('.three').html() === str)) {
+    alert('Player ' + $('#player').text() + '(' + str + ')' + ' wins! Congratulations!!!');
+    setTimeout(reset(), 500);
   }
-  else {
-    $('.box').text('X');
+  else if (($('.four').html() === str) && ($('.five').html() === str) && ($('.six').html() === str)) {
+    alert('Player ' + $('#player').text() + '(' + str + ')' + ' wins! Congratulations!!!');
+    setTimeout(reset(), 500);
+  }
+  else if (($('.seven').html() === str) && ($('.eight').html() === str) && ($('.nine').html() === str)) {
+    alert('Player ' + $('#player').text() + '(' + str + ')' + ' wins! Congratulations!!!');
+    setTimeout(reset(), 500);
+  }
+  else if (($('.one').html() === str) && ($('.four').html() === str) && ($('.seven').html() === str)) {
+    alert('Player ' + $('#player').text() + '(' + str + ')' + ' wins! Congratulations!!!');
+    setTimeout(reset(), 500);
+  }
+  else if (($('.two').html() === str) && ($('.five').html() === str) && ($('.eight').html() === str)) {
+    alert('Player ' + $('#player').text() + '(' + str + ')' + ' wins! Congratulations!!!');
+    setTimeout(reset(), 500);
+  }
+  else if (($('.three').html() === str) && ($('.six').html() === str) && ($('.nine').html() === str)) {
+    alert('Player ' + $('#player').text() + '(' + str + ')' + ' wins! Congratulations!!!');
+    setTimeout(reset(), 500);
+  }
+  else if (($('.one').html() === str) && ($('.five').html() === str) && ($('.nine').html() === str)) {
+    alert('Player ' + $('#player').text() + '(' + str + ')' + ' wins! Congratulations!!!');
+    setTimeout(reset(), 500);
+  }
+  else if (($('.seven').html() === str) && ($('.five').html() === str) && ($('.three').html() === str)) {
+    alert('Player ' + $('#player').text() + '(' + str + ')' + ' wins! Congratulations!!!');
+    setTimeout(reset(), 500);
+  }
+  else if ($('.box').text().length === 9) {
+    alert('Looks like the game is a draw! Try again!')
+    setTimeout(reset(), 500);
   }
 }
 
 
 $(document).ready(function(){
-  $('.box').on('click', varup);
+  $('.box').on('click', countup);
   $('.box').on('click', function(){
     if (count % 2 === 0 && $(this).text().trim() === ''){
         $(this).html('O');
+        $(this).css('background-color', 'mediumblue')
+        setTimeout(function(){checkwin('O')}, 500);
       }
     else if (count % 2 !== 0 && $(this).text().trim() === ''){
         $(this).html('X');
+        setTimeout(function(){checkwin('X')}, 500);
+        $(this).css('background-color', 'firebrick');
       }})
+
+  $('.box').on('click', function(){
+    if ($('#player').html() === '1'){
+      $('#player').html('2');
+    }
+    else if ($('#player').html() === '2') {
+      $('#player').html('1');
+    }
+  })
+
   $('.restart').on('click', function(){
     $('.box').empty();
+    $('#player').html('1');
   });
     });
-
-
-
-
-
-  //Previous solution. Ask TAs how this can be refactored
-  // $('.one').on('click', function(){
-  //   if (count % 2 ===0){
-  //     $('.one').text('O');
-  //   }
-  //   else {
-  //     $('.one').text('X');
-  //   }})
-  // $('.two').on('click', function(){
-  //   if (count % 2 ===0){
-  //     $('.two').text('O');
-  //   }
-  //   else {
-  //     $('.two').text('X');
-  //   }})
-  // $('.three').on('click', function(){
-  //   if (count % 2 ===0){
-  //       $('.three').text('O');
-  //     }
-  //   else {
-  //       $('.three').text('X');
-  //     }});
-  // $('.four').on('click', function(){
-  //   if (count % 2 ===0){
-  //         $('.four').text('O');
-  //       }
-  //   else {
-  //         $('.four').text('X');
-  //       }})
-  // $('.five').on('click', function(){
-  //   if (count % 2 ===0){
-  //         $('.five').text('O');
-  //             }
-  //   else {
-  //         $('.five').text('X');
-  //             }})
-  // });
-  // $('.five').on('click', ttt);
-  // $('.six').on('click', ttt);
-  // $('.seven').on('click', ttt);
-  // $('.eight').on('click', ttt);
-  // $('.nine').on('click', ttt);
